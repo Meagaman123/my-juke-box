89126496ed079337feb7bac6b4aaca7edd67a04a
Meagaman123 <34253582+Meagaman123@users.noreply.github.com>
2018-04-16 00:34:14 +0100

    Add files via upload
    this is the first code

---
+++ my juke box/my juke box/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Genre_List = new System.Windows.Forms.ListBox();
+            this.Genre_Title = new System.Windows.Forms.TextBox();
+            this.cp = new System.Windows.Forms.TextBox();
+            this.Currently_Playing = new System.Windows.Forms.TextBox();
+            this.Select_Genre_Hscroll = new System.Windows.Forms.HScrollBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.setUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Play_List = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Genre_List
+            // 
+            this.Genre_List.FormattingEnabled = true;
+            this.Genre_List.Location = new System.Drawing.Point(87, 113);
+            this.Genre_List.Name = "Genre_List";
+            this.Genre_List.Size = new System.Drawing.Size(200, 82);
+            this.Genre_List.TabIndex = 0;
+            this.Genre_List.SelectedIndexChanged += new System.EventHandler(this.Genre_List_SelectedIndexChanged);
+            // 
+            // Genre_Title
+            // 
+            this.Genre_Title.Location = new System.Drawing.Point(87, 96);
+            this.Genre_Title.Name = "Genre_Title";
+            this.Genre_Title.ReadOnly = true;
+            this.Genre_Title.Size = new System.Drawing.Size(200, 20);
+            this.Genre_Title.TabIndex = 1;
+            // 
+            // cp
+            // 
+            this.cp.Location = new System.Drawing.Point(194, 1);
+            this.cp.Name = "cp";
+            this.cp.Size = new System.Drawing.Size(179, 20);
+            this.cp.TabIndex = 2;
+            this.cp.Text = "Copyright © 2018 Gianluca Nocivelli";
+            this.cp.TextChanged += new System.EventHandler(this.cp_TextChanged);
+            // 
+            // Currently_Playing
+            // 
+            this.Currently_Playing.BackColor = System.Drawing.Color.Lime;
+            this.Currently_Playing.Location = new System.Drawing.Point(101, 244);
+            this.Currently_Playing.Name = "Currently_Playing";
+            this.Currently_Playing.ReadOnly = true;
+            this.Currently_Playing.Size = new System.Drawing.Size(172, 20);
+            this.Currently_Playing.TabIndex = 3;
+            // 
+            // Select_Genre_Hscroll
+            // 
+            this.Select_Genre_Hscroll.Location = new System.Drawing.Point(87, 189);
+            this.Select_Genre_Hscroll.Name = "Select_Genre_Hscroll";
+            this.Select_Genre_Hscroll.Size = new System.Drawing.Size(200, 24);
+            this.Select_Genre_Hscroll.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setUpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 569);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(374, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // setUpToolStripMenuItem
+            // 
+            this.setUpToolStripMenuItem.Name = "setUpToolStripMenuItem";
+            this.setUpToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.setUpToolStripMenuItem.Text = "Set up";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // Play_List
+            // 
+            this.Play_List.BackColor = System.Drawing.SystemColors.Info;
+            this.Play_List.FormattingEnabled = true;
+            this.Play_List.Location = new System.Drawing.Point(87, 291);
+            this.Play_List.Name = "Play_List";
+            this.Play_List.Size = new System.Drawing.Size(200, 108);
+            this.Play_List.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(374, 593);
+            this.Controls.Add(this.Play_List);
+            this.Controls.Add(this.Select_Genre_Hscroll);
+            this.Controls.Add(this.Currently_Playing);
+            this.Controls.Add(this.cp);
+            this.Controls.Add(this.Genre_Title);
+            this.Controls.Add(this.Genre_List);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "My Juke Box v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Genre_Title;
+        private System.Windows.Forms.ListBox Genre_List;
+        private System.Windows.Forms.TextBox cp;
+        private System.Windows.Forms.TextBox Currently_Playing;
+        private System.Windows.Forms.HScrollBar Select_Genre_Hscroll;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem setUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ListBox Play_List;
+    }
+}
+
---
+++ my juke box/my juke box/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                
+                int Int_Number_of_Genera;
+                //initializing the maximun and the minimun of the scroll bar so that when we read the file it will scroll to the maximun value
+
+                Select_Genre_Hscroll.Minimum = 1;
+                Select_Genre_Hscroll.SmallChange = 1;
+                Select_Genre_Hscroll.Maximum = 3;
+                
+
+                //this creates the array that will store the file when we read it in
+                ListBox[] Media_Library;
+
+                
+                //clearing the genra so it shows up empty when the program launches and so that there isnt anything before we start
+                Genre_List.Items.Clear();
+                Genre_Title.Text = "";
+
+
+                //this finds the file directory and then searches for the file 
+                string applicationPath = Directory.GetCurrentDirectory() + "\\";
+
+
+                //this reads the file file stream is a short way of writing it instead of the whole thing.
+                StreamReader Filestream = new StreamReader(applicationPath + ("Media.txt"));
+
+
+                //this gets the first line in the file and converts it into an int puts it as number of genera 
+                Int_Number_of_Genera = Convert.ToInt32(Filestream.ReadLine());
+
+
+                //this takes the int number of genra string and puts it into the array.
+                Media_Library = new ListBox[Int_Number_of_Genera];
+
+
+                //this then goes through the foor loop exactly the amount of number of genra there is in the file it will then read in the next 2 lines and store them in the array 
+                for (int i = 0; i < Int_Number_of_Genera; i++)
+                {
+                   
+                    Media_Library[i] = new ListBox();
+                    int num = Convert.ToInt32(Filestream.ReadLine());
+                    Media_Library[i].Items.Add(Filestream.ReadLine());
+                    //this adds the the text in the file to the text file and displays it 
+                    for (int j = 0; j < num; j++)
+                    {
+                        string item = Filestream.ReadLine();
+                        Media_Library[i].Items.Add(item);
+                    }
+                }
+                Filestream.Close();
+
+                
+                
+
+
+                Genre_Title.Text = Media_Library[Select_Genre_Hscroll.Value-1].Items[0].ToString();
+
+
+
+
+                int Which_Genera = Select_Genre_Hscroll.Value - 1 ;
+
+                Genre_List.Items.Clear();
+                Genre_Title.Text = Media_Library[Which_Genera].Items[0].ToString();
+                for (int i = 1; i <= Media_Library[Which_Genera].Items.Count - 1; i++)
+                {
+                    Genre_List.Items.Add(Media_Library[Which_Genera].Items[i].ToString());
+                }
+
+
+            }
+        }
+
+
+
+        private void Genre_List_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
